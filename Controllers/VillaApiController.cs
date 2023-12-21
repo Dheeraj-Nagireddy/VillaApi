2bdc2dac2f2898301ac10fe872fc8ab30eeaec08
Dheeraj-Nagireddy <nagireddynagadheeraj@gmail.com>
2023-12-21 00:03:41 -0500

    Added ActionResult & Validation for different return types

--- Controllers/VillaApiController.cs
+++ Controllers/VillaApiController.cs
@@ -10,18 +10,33 @@ namespace VillaApi.Controllers
     // [Route("api/[controller]")] --> can use the controller instead of explicity defining the controller name
     public class VillaApiController : ControllerBase //controllerBase doesnot have support for views
     {
+
+// Get list of villas
+        
         [HttpGet]
-        public IEnumerable<VillaDto> GetVillas()
+        public ActionResult<IEnumerable<VillaDto>> GetVillas()
         {
-            return VillaStore.villaList;
-            
+            return Ok(VillaStore.villaList);
         }
-       [HttpGet("id")]
-        public VillaDto GetVilla(int id)
+
+//      Get Villa By Id
+
+        [HttpGet("id")]
+//     [HttpGet("{id:int}")] if id is not written here it will throw an error that the request matched multiple points
+        public ActionResult<VillaDto> GetVilla(int id)
         {
+            if(id==0)
+            {
+                return BadRequest();
+            }
             var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
-            return villa;
-        } 
+            
+            if(villa is null)
+            {
+                return NotFound();
+            }
+            return Ok(villa);
+        }
     }
 
 
